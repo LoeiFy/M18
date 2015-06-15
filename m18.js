4165747653947815f71252dc5bfef03b39830cd5
LoeiFy <loeify@gmail.com>
2015-06-15 23:32:50 +0800

    function

--- m18.js
+++ m18.js
@@ -38,27 +38,53 @@ document.addEventListener('DOMContentLoaded', function() {
     })
 
     enter.addEventListener('click', function() {
+        var cn = chinese.value,
+            en = english.value;
+
         if (!fileData) {
             alert('please select an image')
-            return;
+            return
+        }
+
+        if (cn.length <= 0 || en.length <= 0) {
+            alert('no subtitle')
+            return
         }
 
         reader.readAsDataURL(fileData)
         reader.onload = function(e) {
             image.src = e.target.result;
             image.onload = function() {
-                img2canvas()
+                // render image
+                canvas.width = image.width;
+                canvas.height = image.height;
+
+                context = canvas.getContext('2d');
+                context.drawImage(image, 0, 0)
+
+                // render text
+                context.shadowColor = 'black';
+                context.shadowOffsetX = 1;
+                context.shadowOffsetY = 1;
+                context.shadowBlur = 3;
+                context.fillStyle = 'white';
+
+                // chinese
+                context.beginPath()
+                context.font = 'normal 20px Lucida Grande, Helvetica Neue, Helvetica, Arial';
+
+                var cnArea = context.measureText(cn);
+                context.fillText(cn, canvas.width / 2 - cnArea.width / 2, canvas.height - 20 - 20 * 1.5)
+
+                // english
+                context.beginPath()
+                context.font = 'normal 14px Lucida Grande, Helvetica Neue, Helvetica, Arial';
+
+                var enArea = context.measureText(en);
+                context.fillText(en, canvas.width / 2 - enArea.width / 2, canvas.height - 5 - 14 * 1.5)
             }
         }
     }, false)
-
-    function img2canvas() {
-        canvas.width = image.width;
-        canvas.height = image.height;
-
-        context = canvas.getContext('2d');
-        context.drawImage(image, 0, 0)
-    }
 
     select.addEventListener('dragover', function(e) {
         e.preventDefault()
@@ -78,46 +104,3 @@ document.addEventListener('DOMContentLoaded', function() {
     }, false)
 
 })
-
-/*
-document.getElementById('image').addEventListener('change', function(e) {
-    var reader = new FileReader();
-    reader.readAsDataURL(e.target.files[0])
-
-    reader.onload = function(o) {
-        var image = new Image();
-        image.src = o.target.result;
-
-        image.onload = function() {
-            var canvas = document.getElementById('canvas');
-            canvas.width = image.width;
-            canvas.height = image.height;
-
-            var context = canvas.getContext('2d');
-            context.drawImage(image, 0, 0)
-
-            context.shadowColor = "black";
-            context.shadowOffsetX = 1;
-            context.shadowOffsetY = 1;
-            context.shadowBlur = 3;
-
-            context.beginPath()
-            context.font = 'normal 20px Lucida Grande, Helvetica Neue, Helvetica, Arial';
-            context.fillStyle = 'white';
-            var cnwh = context.measureText(cn);
-            if (cnwh.width >= canvas.width) console.log('too wide');
-            context.fillText(cn, canvas.width / 2 - cnwh.width / 2, canvas.height - 20 - 20 * 1.5)
-
-            context.beginPath()
-            context.font = 'normal 14px Lucida Grande, Helvetica Neue, Helvetica, Arial';
-            context.fillStyle = 'white';
-            var enwh = context.measureText(en);
-            if (enwh.width >= canvas.width) console.log('too wide');
-            context.fillText(en, canvas.width / 2 - enwh.width / 2, canvas.height - 5 - 14 * 1.5)
-
-            var dataURL = canvas.toDataURL();
-            document.getElementById('download').href = dataURL;
-        }
-    } 
-}, false)
-*/
